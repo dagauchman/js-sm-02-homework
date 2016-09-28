13ba11c71501f96fa07d4afadcbd2d14d156e021
dagauchman <dagauchman@gmail.com>
2016-09-27 20:11:31 -0700

    updated during class

--- Week 03 Homework/Random Address Generator - Starter Code/js/app.js
+++ Week 03 Homework/Random Address Generator - Starter Code/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 
-	var houseNumber = ['206', '501', '456', '5787', '6654', '2244', '5566', '8910', '10', '20', '307' ];
 	var streets = ['Main', 'Smith', 'Wall', '42nd', 'Astor', 'St. Marks', 'Broadway', 'Water', 'Park', '5th', 'Hollywood' ];
 	var types = ['St', 'Ave', 'Way', 'Hwy', 'Place'];
 	var states = ['NY', 'CA', 'TN', 'TX', 'MO'];
@@ -11,11 +10,8 @@ $(document).ready(function(){
 
 	$('button#makeAddress').click(function(){
 
-		// Math.floor((Math.random() * 10) + 1); 
 
-		// console.log( Math.floor(Math.random() * 10 + 1));
-		var houseRandom = Math.floor(Math.random() * houseNumber.length);
-
+		var houseRandom = Math.floor(Math.random() * 10000);
 		var streetRandom = Math.floor(Math.random() * streets.length);
 		var typesRandom = Math.floor(Math.random() * types.length);
 		var statesRandom = Math.floor(Math.random() * states.length);
@@ -23,11 +19,9 @@ $(document).ready(function(){
 		var zipRandom = Math.floor(Math.random() * zip.length);
 
 		
-		// console.log( streets[streetRandom ]);
 
-		//will generate a random number between 1 - 10.
 
-		$('address.destination').html( houseNumber[houseRandom] + ' ' + streets[streetRandom]+ ' ' + types[typesRandom] + '<br/>' );
+		$('address.destination').html( houseRandom + ' ' + streets[streetRandom]+ ' ' + types[typesRandom] + '<br/>' );
 		$('address.destination').append( cities[citiesRandom] + ', ' + states[statesRandom] + ' ' + zip[zipRandom] );
 
 	});
